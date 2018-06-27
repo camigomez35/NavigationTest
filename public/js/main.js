ad1eec2844fec0a9bc7249ab76f93dea93607dcd
Maria Camila Gómez <camigomez@experimentality.co>
2018-06-26 23:25:20 -0500

    Add toggle navigation secondary

--- public/js/main.js
+++ public/js/main.js
@@ -8,7 +8,7 @@ function showItems() {
     menu = createNavigation(data.items, 1)
     navbar.innerHTML = logo + menu
 
-    var items = document.getElementsByClassName('nav-item');
+    var items = document.getElementsByClassName('nav__item nav__item_list');
     for (var count = 0; count < items.length; count++) {
         if (items.item(count).childNodes.length > 1) {
             items.item(count).addEventListener('click', toogleSubMenu);
@@ -50,15 +50,35 @@ function createlist(items) {
         var childItems = item.items && item.items.length;
         var li = '';
         if (childItems) {
-            li += '<li class="nav__item"> ';
-            li += '<a id="' + item.label + '">' + item.label + "</a>" + createNavigation(item.items, 2);
+            li += '<li class="nav__item nav__item_list"> ';
+            li += '<a id="' + item.label + '">' + item.label + '<span class="nav__item_arrow"/></span></a>' + createNavigation(item.items, 2);
         } else {
             li += '<li class="nav__item"> ';
-            li += '<a id="' + item.label + '" href="' + item.url + '">' + item.label + "</a>";
+            li += '<a id="' + item.label + '" href="' + item.url + '">' + item.label + '</a>';
         }
         li += '</li>'
         menu += li;
     }
 
     return menu
+}
+
+function toogleSubMenu() {
+    var oldItem = document.getElementsByClassName('nav__item nav__item_list nav__item--open')[0];
+    if (oldItem) {
+        removeClass(oldItem, 'nav__item--open');
+        removeClass(document.getElementsByTagName("BODY")[0], 'body--open');
+    }
+    if (this && oldItem !== this) {
+        this.className += ' nav__item--open';
+        document.getElementsByTagName("BODY")[0].className += 'body--open'
+        // removeClass(document.getElementsByTagName("BODY")[0], 'body--open');
+    }
+}
+
+function removeClass(element, className) {
+    var classArray = element.className.split(' ');
+    var index = classArray.indexOf(className);
+    classArray.splice(index, 1);
+    element.className = classArray.join(' ');
 }
